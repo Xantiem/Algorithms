dbab9835aaa591cb9e81830058cf40bb5ecd6efe
Nicholas <35278540+Xantiem@users.noreply.github.com>
2024-11-08 01:37:39 +0100

    Add files via upload

---
+++ deviation_from_mean.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <float.h>
+
+// DBL_MIN (minimum positive value)  
+// DBL_MAX (maximum negative value) 
+// DBL_DIG (number of digit precision)
+
+extern double _deviation_from_mean(double x, double mean);
+
+double deviation_from_mean(double x, double mean) {
+    return (x - mean);
+}
+
+int main() {
+
+    double a = 10.2;
+    double b = 3.6;
+    double result = _deviation_from_mean(a,b);
+
+    printf("%.2f", result);
+
+    return 0;
+}
